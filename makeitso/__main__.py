a87798b510294b7229254ef07b0febb625f4143d
C. Titus Brown <titus@idyll.org>
2017-07-14 07:05:06 -0700

    server works, sendtask

--- makeitso/__main__.py
+++ makeitso/__main__.py
@@ -1,15 +1,20 @@
 #! /usr/bin/env python
 import argparse
+import sys
+import requests
+from requests.auth import HTTPDigestAuth
+import json
 
-from .logging import *
+from .logging import notify, set_quiet
 from .find_cwldef import find_cwldef
 from .call_cwl import call_cwl
+from .server import app
 
 
 def main():
     set_quiet(False)
 
-    commands = {'run': run }
+    commands = {'run': run, 'server': server, 'sendtask': sendtask}
     parser = argparse.ArgumentParser(
         description='do things with CWL',
         usage='''makeitso <command> [<args>]
@@ -17,10 +22,13 @@ def main():
 Commands can be:
 
 run                    run CWL workflow on current computer
+server                 run a task pool server
+worker                 run a task pool worker
+sendtask               send a task to the server
 
 Use '-h' to get subcommand-specific help, e.g.
 
-sourmash compute -h
+makeitso run -h
 .
 ''')
     parser.add_argument('command')
@@ -46,6 +54,34 @@ def run(args):
     notify('params: {}'.format(args.params_yaml))
 
     call_cwl(task, args.params_yaml)
+
+
+def server(args):
+    p = argparse.ArgumentParser()
+    p.add_argument('-p', '--port', type=int, default=5000)
+    args = p.parse_args(args)
+
+    app.run(debug=True, port=args.port)
+
+
+def sendtask(args):
+    p = argparse.ArgumentParser()
+    p.add_argument('-S', '--server-url', default='http://localhost:5000')
+    p.add_argument('taskname')
+    p.add_argument('params')
+    args = p.parse_args(args)
+
+    full_url = args.server_url + '/todo/api/v1.0/tasks'
+
+    # load up the payload
+    params_content = open(args.params).read()
+    taskdict = dict(taskname=args.taskname, params=params_content)
+
+    # authenticate & POST
+    auth = HTTPDigestAuth('miguel', 'python')
+    response = requests.post(full_url, json=taskdict, auth=auth)
+
+    response.raise_for_status()
 
 
 if __name__ == '__main__':
---
+++ makeitso/server.py
@@ -0,0 +1,92 @@
+#!flask/bin/python
+from flask import Flask, jsonify, abort
+from flask import make_response
+from flask import request
+from flask import url_for
+from flask_httpauth import HTTPBasicAuth
+
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
+
+app = Flask('makeitso')
+
+tasks = [
+    {
+        'id': 1,
+        'taskname': u'ctb/dockstore-tool-bamstats',
+        'params': u'',
+        'done': False,
+    },
+]
+
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
+def get_tasks():
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+        task = [task for task in tasks if task['id'] == task_id]
+        if len(task) == 0:
+            abort(404)
+        return jsonify({'task': task[0]})
+
+
+@app.errorhandler(404)
+def not_found(error):
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'taskname' in request.json \
+             or not 'params' in request.json:
+        abort(400)
+
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'taskname': request.json.get('taskname', ""),
+        'params': request.json.get('params', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'taskname' in request.json:
+        abort(400)
+    if 'params' in request.json:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
